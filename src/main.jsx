41c490b17555ef0cb72cfec1b351a3b17c701658
Lucaslonski-code <lucaslonski890@gmail.com>
2026-05-03 20:49:02 -0300

    instalação do react-router e configuração incial de rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { GlobalStyle } from './styles/GlobalStyles.js'
-import Home from './pages/Home/index.jsx' //import default
+import { RouterProvider } from 'react-router-dom'
+import router from './routes.jsx' // importação de componentes, arquivos, imagens, etc
+
+// import Home from './pages/Home/index.jsx' //import default
 // import { App, App2 as Appp } from './App.jsx' //import nomeado
 // necessário {}, e todos os elementos exportados, ou seja, o nome tem que ser o mesmo do export,
 //  ou usar "as" para renomear.
@@ -9,6 +12,6 @@ import Home from './pages/Home/index.jsx' //import default
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GlobalStyle />
-    <Home />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
