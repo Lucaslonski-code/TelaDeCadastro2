4a47811e42bb6683ee0f70e38b2e2aef6036873c
Lucaslonski-code <lucaslonski890@gmail.com>
2026-05-05 12:14:35 -0300

    Instalação inicial do useEffect, usuários encontrados com sucesso

--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -1,8 +1,21 @@
 import Button from '../../components/Button/'
 import TopBackground from '../../components/TopBackground'
 import { useNavigate } from 'react-router-dom'
+import api from '../../services/api'
+import { useEffect } from 'react'
 
 function ListUsers() {
+
+    //TODA VEZ que a tela carrega, o useEffect é chamado, e dentro do useEffect tem a função que busca os usuários cadastrados, e depois exibe eles na tela.
+    //TODA VEZ que uma variavel muda, o useEffect é chamado, e dentro do useEffect tem a função que busca os usuários 
+
+    useEffect(() => {
+        async function getUsers() {
+            const usersFromApi = await api.get('/usuarios')
+            console.log(usersFromApi)
+        }
+        getUsers()
+    }, [])
 
     const navigate = useNavigate()
 
